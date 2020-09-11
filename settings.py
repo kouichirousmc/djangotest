2880bf6d430ce9a7375f869eb36d156e5737765b
GIS10 <WADA.KOUICHIROU@SMCJPN.CO.JP>
2020-09-11 14:23:58 +0900

    setting

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+2p+ll_9ifdb^%*ht4m4db34ola+xx5=fyr!6#y6i9)5f^wmy8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
